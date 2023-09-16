971240630ce6ece162a7a1cc02454ed7e416dbea
sangwoong <sangwoong03@gmail.com>
2023-09-16 23:42:33 +0900

    Add: Setup MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { PingController } from './ping/ping.controller';
 import { PingService } from './ping/ping.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.DB_URL),
+  ],
   controllers: [PingController],
   providers: [PingService],
 })
