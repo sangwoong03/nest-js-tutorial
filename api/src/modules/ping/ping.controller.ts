e35ada271becbe840e2bbc7ca87983ac524c2ebd
sangwoong <sangwoong03@gmail.com>
2023-11-24 18:41:19 +0900

    feat: Added guard test

--- api/src/modules/ping/ping.controller.ts
+++ api/src/modules/ping/ping.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Delete, Patch, Post } from '@nestjs/common';
+import { Controller, Get, Delete, Patch, Post, UseGuards } from '@nestjs/common';
 import { PingService } from './ping.service';
 import Ping from './entity/ping.entity';
+import { AuthGuard } from 'src/common/guard/auth.guard';
 
 @Controller('ping')
 export class PingController {
   constructor( private readonly pingService: PingService ) { }
 
   @Get()
+  // test guards
+  // @UseGuards(AuthGuard)
   getPing(): Ping {
     return this.pingService.pong()
   }
